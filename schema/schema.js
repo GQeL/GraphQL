6207a0bf51dbb45535482b935f61a94c6b92fc79
Americanman <snowboardracer7@gmail.com>
2021-05-23 12:10:31 -0400

    Fetch date from third party service

--- schema/schema.js
+++ schema/schema.js
@@ -1,17 +1,11 @@
 const graphql = require('graphql');
-const _ = require('lodash')
+const axios = require('axios')
 const {
 	GraphQLObjectType,
 	GraphQLString,
 	GraphQLInt,
 	GraphQLSchema
 } = graphql;
-
-// temporary hardcoded users:
-const users = [
-	{ id: '23', firstName: 'Bill', age: 20 },
-	{ id: '47', firstName: 'Samantha', age: 21 }
-]
 
 const UserType = new GraphQLObjectType({
 	name: 'User',
@@ -31,7 +25,8 @@ const RootQuery = new GraphQLObjectType({
 			type: UserType,
 			args: { id: { type: GraphQLString } },
 			resolve(parentValue, args) {
-				return _.find(users, { id: args.id })
+				return axios.get(`http://localhost:3000/users/${args.id}`)
+					.then(resp => resp.data)
 			}
 		}
 	}
